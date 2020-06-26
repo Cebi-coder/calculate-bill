5045a99848713d83d550146749d2262d9875192a
Cebi-coder <cebisapeter40@gmail.com>
2020-06-26 17:29:24 +0200

    create tests for bill wih settings

---
+++ calculate-bill-factory-function.js
@@ -0,0 +1,20 @@
+function calculateBill(){
+    
+    var billString; 
+    
+    var totalBill = 0
+
+    
+}
+
+function callPrice() {
+    billString = 
+
+}
+
+function smsPrice(){
+    "sms" = 0.75
+}
+
+
+btn.addEventListener('click', calculateBill);
